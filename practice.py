e2d50d5e4c4be3d66c8bfc6d64fd64cb5f42d541
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 14:20:39 +0300

    First version of assistant added.

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print('Hello world!')
-print('absolutely new line here')
-print('pipa')
+
+
+def bia(dice_roll, goal=None):
+    """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
+    if dice_roll >= 17:
+        print('No bardic inspiration dice needed')
+    else:
+        if goal:
+            bardic_dice_max_value = 6
+            if goal - dice_roll <= bardic_dice_max_value:
+                print('Use bardic inspiration dice')
+            else:
+                print('No bardic inspiration dice needed')
+        else:
+            print("It's risky, dude!!!")
+
+bia(12, 6)
+
