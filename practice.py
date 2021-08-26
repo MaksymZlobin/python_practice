389633b8d997f334ee126e20e2acef77f9137d89
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 16:03:55 +0300

    Changed repeated parts of code.

--- practice.py
+++ practice.py
@@ -3,14 +3,14 @@ bardic_dice_max_value = 6
 
 def bardic_inspiration_assistant(dice_roll, goal=None):
     """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
-    if dice_roll >= 17:
-        return 'No bardic inspiration dice needed'
-    else:
-        if goal:
-            if goal - dice_roll <= bardic_dice_max_value:
-                return 'Use bardic inspiration dice'
-        else:
-            return "It's risky, dude!!!"
+    bardic_formula = goal - dice_roll <= bardic_dice_max_value if goal else False
+    x = dice_roll >= goal if goal else False
+    if dice_roll >= 17 or (goal and not bardic_formula) or x:
+        return 'No bardic inspiration dice needed'_1
+    elif goal and bardic_formula:
+        return 'Use bardic inspiration dice'io_2
+    elif not goal:
+        return "It's risky, dude!!!"
 
 
 print(bardic_inspiration_assistant(16, 24))
