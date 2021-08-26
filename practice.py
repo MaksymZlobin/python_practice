594d999df7588b73f442001f1f1bbef9bd467933
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 18:04:36 +0300

    Data types validation fixed.

--- practice.py
+++ practice.py
@@ -6,11 +6,11 @@ APPROPRIATE_DICE_ROLL = 17
 
 def bardic_inspiration_assistant(dice_roll: int, goal: Optional[int] = None) -> str:
     """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
+    if type(dice_roll) != int or type(goal) not in [int, type(None)]:
+        return 'Invalid data type!'
     bardic_formula = goal - dice_roll <= BARDIC_DICE_MAX_VALUE if goal else False
     is_dice_roll_gte_goal = dice_roll >= goal if goal else False
-    if type(dice_roll) != int or type(goal) not in [int, None]:
-        return 'Invalid data type!'
-    elif dice_roll >= APPROPRIATE_DICE_ROLL or (goal and not bardic_formula) or is_dice_roll_gte_goal:
+    if dice_roll >= APPROPRIATE_DICE_ROLL or (goal and not bardic_formula) or is_dice_roll_gte_goal:
         return 'No bardic inspiration dice needed'
     elif goal and bardic_formula:
         return 'Use bardic inspiration dice'
@@ -18,4 +18,4 @@ def bardic_inspiration_assistant(dice_roll: int, goal: Optional[int] = None) -> 
         return "It's risky, dude!!!"
 
 
-print(bardic_inspiration_assistant('pipa'))
+print(bardic_inspiration_assistant())
