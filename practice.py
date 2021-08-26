fdf58bde156322d14946cc9e763c3149783599ec
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 15:08:52 +0300

    Changed prints on returns in code.

--- practice.py
+++ practice.py
@@ -4,13 +4,13 @@ bardic_dice_max_value = 6
 def bardic_inspiration_assistant(dice_roll, goal=None):
     """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
     if dice_roll >= 17:
-        print('No bardic inspiration dice needed')
+        return 'No bardic inspiration dice needed'
     else:
         if goal:
             if goal - dice_roll <= bardic_dice_max_value:
-                print('Use bardic inspiration dice')
+                return 'Use bardic inspiration dice'
         else:
-            print("It's risky, dude!!!")
+            return "It's risky, dude!!!"
 
-bardic_inspiration_assistant(17)
 
+print(bardic_inspiration_assistant(16, 24))
