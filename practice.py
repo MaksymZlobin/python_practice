6264eb384eccba1096fddfd3570f35c809fcf098
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 17:29:06 +0300

    Separate constants for custom values added. Conditions improved.

--- practice.py
+++ practice.py
@@ -1,15 +1,16 @@
 from typing import Optional
 
-bardic_dice_max_value = 6
+BARDIC_DICE_MAX_VALUE = 6
+APPROPRIATE_DICE_ROLL = 17
 
 
 def bardic_inspiration_assistant(dice_roll: int, goal: Optional[int] = None) -> str:
     """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
-    bardic_formula = goal - dice_roll <= bardic_dice_max_value if goal else False
-    x = dice_roll >= goal if goal else False
+    bardic_formula = goal - dice_roll <= BARDIC_DICE_MAX_VALUE if goal else False
+    is_dice_roll_gte_goal = dice_roll >= goal if goal else False
     if type(dice_roll) != int or type(goal) not in [int, None]:
         return 'Invalid data type!'
-    elif dice_roll >= 17 or (goal and not bardic_formula) or x:
+    elif dice_roll >= APPROPRIATE_DICE_ROLL or (goal and not bardic_formula) or is_dice_roll_gte_goal:
         return 'No bardic inspiration dice needed'
     elif goal and bardic_formula:
         return 'Use bardic inspiration dice'
