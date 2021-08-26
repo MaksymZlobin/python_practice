bc185b467746ff4a0f6e5a3fe8d16528ba273792
Maksym Zlobin <maksym.zlobin.python@gmail.com>
2021-08-26 16:36:50 +0300

    Added data type checks.

--- practice.py
+++ practice.py
@@ -1,16 +1,20 @@
+from typing import Optional
+
 bardic_dice_max_value = 6
 
 
-def bardic_inspiration_assistant(dice_roll, goal=None):
+def bardic_inspiration_assistant(dice_roll: int, goal: Optional[int] = None) -> str:
     """"D&D bardic inspiration dice assistant, that make you choose correct decision"""
     bardic_formula = goal - dice_roll <= bardic_dice_max_value if goal else False
     x = dice_roll >= goal if goal else False
-    if dice_roll >= 17 or (goal and not bardic_formula) or x:
-        return 'No bardic inspiration dice needed'_1
+    if type(dice_roll) != int or type(goal) not in [int, None]:
+        return 'Invalid data type!'
+    elif dice_roll >= 17 or (goal and not bardic_formula) or x:
+        return 'No bardic inspiration dice needed'
     elif goal and bardic_formula:
-        return 'Use bardic inspiration dice'io_2
+        return 'Use bardic inspiration dice'
     elif not goal:
         return "It's risky, dude!!!"
 
 
-print(bardic_inspiration_assistant(16, 24))
+print(bardic_inspiration_assistant('pipa'))
